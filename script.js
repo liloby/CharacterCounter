b1ebc82bce0733e082d776d17bcee8628e34e6de
Oby <obyli214@gmail.com>
2022-09-15 15:35:02 -0700

    character counter functional

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const hex = document.querySelector('.hex')
-const btn = document.querySelector('.generate')
-const box = document.querySelector('.counter-div')
+const count = document.querySelector('.count')
+const input = document.querySelector('input')
 
-const generateColor = () => {
-  let randomColor = Math.random().toString(16).substring(2, 8)
-  document.body.style.backgroundColor = "#" + randomColor
-  hex.innerHTML = "#" + randomColor
-  box.classList.add('stylish')
-}
 
-btn.addEventListener('click', generateColor)
+input.addEventListener("keyup", () => {
+  count.innerHTML = input.value.length;
+})
